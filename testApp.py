dad8c14fdb2d8f76c08776d1f0b485b781c56a24
Jonathan Ohop <jonathan.ohop001@outlook.com>
2025-06-22 05:02:57 -0400

    added spinner gif between search results

--- testApp.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/lookup')
-def lookup():
-    return jsonify({"message": "Lookup route is working"})
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
